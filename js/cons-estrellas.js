d240d3f00ee45d1a140355400e1a685eaf3850d2
keisy07 <naas22@gmail.com>
2016-01-23 17:35:50 -0600

    validacionesEstrellas

--- js/cons-estrellas.js
+++ js/cons-estrellas.js
@@ -1,21 +1,78 @@
+var validateInputs = function () {
+  var result = true,
+    $inputNombre = $('#inp-estrella-nombre'),
+    $inputTemperatura = $('#inp-estrella-temp'),
+    $inputEdad = $('#inp-estrella-edad');
+    $inputIntensidad = $('#inp-estrella-luminosidad'),
+    $inputMasa= $('#inp-estrella-masa'),
+    $inputDuracion = $('#inp-estrella-duracion'),
+    $inputComposicion = $('#inp-estrella-comp'),
+    $inputTamaño = $('#inp-estrella-tam'),
+
+  $('.form-group').removeClass('has-error');
+
+  if (!/^[a-zA-Z]+$/.test($inputNombre.val())) {
+    $inputNombre.closest('.form-group').addClass('has-error');
+    result = false;
+  }
+
+   if (!/^[0-9]+$/.test($inputTemperatura.val())) {
+    $inputTemperatura.closest('.form-group').addClass('has-error');
+    result = false;
+  }
+
+   if (!/^[0-9]+$/.test($inputEdad.val())) {
+    $inputEdad.closest('.form-group').addClass('has-error');
+    result = false;
+  }
+
+  if (!/^[0-9]+$/.test($inputIntensidad.val())) {
+    $inputIntensidad.closest('.form-group').addClass('has-error');
+    result = false;
+  }
+
+  if (!/^[0-9]+$/.test($inputMasa.val())) {
+    $inputMasa.closest('.form-group').addClass('has-error');
+    result = false;
+  }
+
+  if (!/^[0-9]+$/.test($inputDuracion.val())) {
+    $inputDuracion.closest('.form-group').addClass('has-error');
+    result = false;
+  }
+
+   if (!/[A-Za-z0-9_]/.test($inputComposicion.val())) {
+    $inputComposicion.closest('.form-group').addClass('has-error');
+    result = false;
+  }
+
+   if (!/^[0-9]+$/.test($inputTamaño.val())) {
+    $inputTamaño.closest('.form-group').addClass('has-error');
+    result = false;
+  }
+
+  return result;
+};
+
+
 $('document').ready(function () {
   $('input').on('blur', function () {
-    //validateInputs();
+    validateInputs();
   });
 
   $('#reg-estrella').on('click',function(){
     var nuevaFila, valNombre, valTemperatura, valEdad, valIntensidad, valMasa, valDuracion, valComposicion, valTamaño;
 
-
+     if (validateInputs()) {
       valNombre = $('#inp-estrella-nombre').val();
-      valTemperatura= $('#inp-estrella-temp').val();
-      valEdad = $('#inp-estrella-edad').val();
-      valIntensidad = $('#inp-estrella-luminosidad').val();
       valMasa = $('#inp-estrella-masa').val();
-      valDuracion = $('#inp-estrella-duracion').val();
-      valComposicion = $('#inp-estrella-comp').val();
       valTamaño = $('#inp-estrella-tam').val();
-
+      valEdad = $('#inp-estrella-edad').val();
+      valComposicion = $('#inp-estrella-comp').val();
+      valTemperatura= $('#inp-estrella-temp').val();
+      valDuracion = $('#inp-estrella-duracion').val();
+      valIntensidad = $('#inp-estrella-luminosidad').val();
+    
       nuevaFila = '<tr>';
 
       nuevaFila += '<td>' + valNombre + '</td>';
@@ -31,7 +88,7 @@ $('document').ready(function () {
 
       $('#tabla-estrellas tbody').append(nuevaFila);
 
-   
+    }
   });
 
 });
