81acc52cb6f2b747ff56c8aab785a0e581c96226
Franced Andres Latouche Ferreto <Franced Andres Latouche Ferreto>
2016-01-24 15:51:42 -0600

    actualizar

--- js/form.js
+++ js/form.js
@@ -112,3 +112,51 @@ var validateInputs = function () {
 
   return result;
 };
+
+
+//Validaciones Form Planetas  
+
+var validateInputs = function () {
+  var result = true,
+    $inputNombre = $('#inp-planeta-nombre'),
+    $inputTemperatura = $('#inp-planeta-temp'),
+    $inputDistancia = $('#inp-planeta-distancia');
+    $inputMasa= $('#inp-planeta-masa'),
+    $inputDuracionDia = $('#inp-planeta-durDia'),
+    $inputDuracionAnno = $('#inp-planeta-durAnno');
+
+  $('.form-group').removeClass('has-error');
+
+  if (!/^[a-zA-Z]+$/.test($inputNombre.val())) {
+    $inputNombre.closest('.form-group').addClass('has-error');
+    result = false;
+  }
+
+   if (!/^[0-9]+$/.test($inputTemperatura.val())) {
+    $inputTemperatura.closest('.form-group').addClass('has-error');
+    result = false;
+  }
+
+   if (!/^[0-9]+$/.test($inputEdad.val())) {
+    $inputEdad.closest('.form-group').addClass('has-error');
+    result = false;
+  }
+
+  if (!/^[0-9]+$/.test($inputIntensidad.val())) {
+    $inputMasa.closest('.form-group').addClass('has-error');
+    result = false;
+  }
+
+  if (!/^[0-9]+$/.test($inputMasa.val())) {
+    $inputDuracionDia.closest('.form-group').addClass('has-error');
+    result = false;
+  }
+
+  if (!/^[0-9]+$/.test($inputDuracion.val())) {
+    $inputDuracionAnno.closest('.form-group').addClass('has-error');
+    result = false;
+  }
+
+
+  return result;
+};
